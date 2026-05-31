259c75cd4a8dceead17e142fcc9a2749d7724029
TibetanMonke <getit.shorts@gmail.com>
2026-05-31 15:27:10 +0200

    Update main.py
    Added the first choice (Chapter 1)

--- main.py
+++ main.py
@@ -7,11 +7,32 @@ time.sleep(2) #change to longer later
 print("\nSubterraneans")
 time.sleep(2)
 
-t=1
+t = 1
 s = 2
 while s > 0.35:
     print(".")
     time.sleep(s)
     t+=0.5
     s = 2/t
-print("insert text")
+print("Chapter 1: Stuck")
+time.sleep(3)
+print("Here you are, in the marshes, you look around and see shallow brown water, plants in the water, plants on the water. There are mosquitoes everywhere.\nYou've got a mere three options: ")
+ 
+choice1 = "1"
+while choice1 != "2":
+    choice1 = input("[1] Turn back to where you came from\n[2] Wade on through the dirty waters\n[3] Drown\n")
+    print(choice1)
+    if choice1 == "1" or choice1 == "2" or choice1 == "3":
+        if choice1 == "1":
+            print("You turn 180 degrees, but see a big dirt wall behind you. You can't turn back.")
+        if choice1 == "3":
+            print("You drowned in the dirty waters. May you rest in peace.")
+            quit()
+            break
+    else:
+        print("Choose an available option")
+        continue
+else:
+    print("You keep on wading through the water, your feet sinking in the mud, your head and shoulders getting repeatedly stung by the many mosquitoes.")
+
+
