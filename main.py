8919c4b7970d64b01a1d2e6806ffd97f6b82b0f1
TibetanMonke <getit.shorts@gmail.com>
2026-05-31 09:20:39 +0200

    Update main.py
    Added a possible start of the game

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 import pygame
-#test
+
+name = input("How would you like to name your character? ")
+print("The story of " + name + " starts here.\nIn the grey lands of the Southern Marshes, where the big beat sounds.\nWhere the Crawling King Snake rules, though he is old and his skin is cold.\nIn these marshes, the peace frogs dance jollily to the big beat.\nThis is where the story starts.")
