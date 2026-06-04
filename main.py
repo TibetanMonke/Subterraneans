d8eb0e7639f3896fbe71278c995eab18bf8931fd
TibetanMonke <getit.shorts@gmail.com>
2026-06-04 11:42:23 +0200

    3-6 13:00
    Klein beetje dialoog toegevoegd

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ while choice1 != "2":
         if choice1 == "3":
             print("You drowned in the dirty waters. May you rest in peace.")
             quit()
-            break
+            break 
     else:
         print("Choose an available option")
         continue
@@ -47,4 +47,6 @@ time.sleep(1)
 print("Before you, a face emerges out of the rain. [insert description of Crawling King Snake]. He asks you: 'Where did you come from and how dare you enter my Southern Marshes?\nYou conjure up a reply:\n")
 
 answer1 = "1"
+if answer1 == "1":
+    print("The Crawling King Snake says: 'Ah, you are one of those, a stranger from the top of the soil. Well, as long as you don't disturb our peace, we won't bother you.'")
 answer1 = input("[1] You say: 'I tripped and fell into a dark pit, falling for what felt like hours, landing in your marshes in front of a dirt wall.' (Philosophical truth)\n[2] You say: 'I'm not sure where I came from, I didn't know these were your marshes, sorry.' (truth)\n[3] You say: 'May peace be upon you and your lands, I am a traveller from the country of sunken mountains and fallen trees, greetings.' (made up philosophical reply with some kind of truth in it)\n[4] You say: 'I walked in here, in search of King Crimson's treasure, do you know where it might be?' (lie)")
