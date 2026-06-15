91c683b8f15fa16eb8d547619cde977d5fa87082
TibetanMonke <getit.shorts@gmail.com>
2026-06-15 22:22:10 +0200

    15-6 22:20
    Minor fixes, made game application

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pygame
 import time
 
 
@@ -21,30 +20,30 @@ def snakeMeeting():
     time.sleep(8)
     print("The hissing forms a song that is haunting but at the same time groovy and that takes you away to higher dimentions.\n")
     time.sleep(8)
-    print("Before you, a face emerges out of the rain. It's a giant king cobra. \nThe snake wears a royal crown on his head, from underneath which long brown curled strands of thick hair fall over the cobra's hood. \nThe cobra asks you: 'Where did you come from and how dare you enter my Southern Marshes?'\n")
+    print("Before you, a face emerges out of the rain. It's a giant king cobra. \nThe snake wears a royal crown on his head, from underneath which long, brown, curled strands of thick hair fall over the cobra's hood. \nThe cobra asks you: 'Where did you come from and how dare you enter my Southern Marshes?'\n")
     time.sleep(20)
     print("You conjure up a reply:\n")
 
 def crystalShipDialogue():
-    print("The Crawling King Snake takes you to a small wooden dock.\nOn the very end of the dock, a rusty old car with no wheels is parked in the shallow water.\nAs you come closer, you see three muddy, brown frogs sitting inside the car2. The driver-frog is wearing glasses.\n")
-    time.sleep(20)
-    print("The one frog sitting a back seat says: 'Hey man... get in.'\n")
+    print("The Crawling King Snake takes you to a small wooden dock.\nOn the very end of the dock, a rusty old car with no wheels is parked in the shallow water.\nAs you come closer, you see three muddy, brown frogs sitting inside the car. The driver-frog is wearing glasses.\n")
+    time.sleep(15)
+    print("The one frog sitting in the back says: 'Hey man... get in.'\n")
     time.sleep(5)
     print("You open the rusty door and step inside as a nauseating smell enters your nose.\nThe driver-frog, with the glasses, turns to you and says: 'Hi.. We're the Riders... on the storm, welcome to our Crystal Ship. We heard we gotta take you outta here.'\nThe shotgun-frog hands you a small, green fruit and says: 'Here take this, eat it. It's the only way outta here man...'\n")
     time.sleep(20)
     print("You hesitantly take the fruit.\nJust as you open your mouth to start chewing, the Crawling King Snake speaks and says: 'Alright now, take it easy. Fare you well Stranger, soil-marcher.'\n")
     time.sleep(10)
-    print("You put the fruit in your mouth and chew.\nAn orange glow slowly forms around the edges of your view.\nThe driver-frog, with the glasses, yells: 'Alright! Let's go on a moonlight drive!'\n")
+    print("You put the fruit in your mouth and chew.\nAn orange glow slowly forms around the edges of your view.\nThe driver-frog, with the glasses, yells: 'Alright! Let's go on a moonlight drive!'")
     time.sleep(10)
 
 def journeyToKingdom():
-    print("You," + name + ", fly over the clouds, inside the Crystal Ship, to lands unknown.")
+    print("You, " + name + ", fly over the clouds, inside the Crystal Ship, to lands unknown.")
     time.sleep(5)
     print("The clouds below you are in every colour and in shapes like in a kaleidoscope.")
     time.sleep(5)
     print("The faces of the frogs around you change proportions constantly.")
     time.sleep(4)
-    print("As you seem to slowly turn back to normal, the Crystal Ship starts descending into the now bright white clouds.")
+    print("As as the trip nears its end, the Crystal Ship starts descending into the now bright white clouds.")
     time.sleep(8)
     print("Looking below you, you see a landscape of seemingly everlasting green hills in all directions.")
     time.sleep(8)
@@ -52,18 +51,18 @@ def journeyToKingdom():
     time.sleep(8)
     print("The driver-frog, with the glasses, says: 'Here you are, had a nice flight? Good luck with whatever you Strangers are trying to do...'")
     time.sleep(8)
-    print("The moment both of your feet are on the grass, the Crystal Ship flies away with an incredible speed.")
+    print("The moment both of your feet are on the grass, the Crystal Ship flies away with an incredible speed.\n")
     time.sleep(8)
-    print("What will you do now?\n")
+    print("What will you do now?")
 
 def moonchildMeeting():
-    print("After a short walk, you close in on the Willow.\nYou realise there is someone there, a child it seems.\nThe child is dancing in the lake and then suddenly runs to sit behind the Willow.\nYou walk up to her and ask her what she's doing.\n")
+    print("After a short walk, you close in on the Willow.\nYou realise there is someone there, a child it seems.\nThe child is dancing in the lake and then suddenly runs to the Willow to sit behind it.\nYou walk up to her and ask her what she's doing.\n")
     time.sleep(20)
     print("The child, not at all fazed by your arrival, replies, whispering:\n 'Shh! I'm playing hide and seek with the Ghosts of Dawn!\nAlthough just then I was dancing in the shallows of a river and in a moment I will be dreaming in the shadows of a Willow.'\n")
     time.sleep(20)
-    print("She seems to have forgotten about the game she's playing, for she continues:\n'You must be one of those Strangers that pass by here occasionaly, I'm the Moonchild, still waiting for a smile of a Sunchild.\nI'll give you some advice: be careful with the King's Gaurds, you mustn't lie to them!'\n")
+    print("She already seems to have forgotten about the game she's playing, for she continues:\n'You must be one of those Strangers that pass by here occasionaly, I'm the Moonchild, still waiting for a smile of a Sunchild.\nI'll give you some advice: be careful with the King's Gaurds, you mustn't lie to them!'\n")
     time.sleep(20)
-    print("Suddenly the Moonchild runs away into the hills and dissapears behind one soon after...\nSo, now what? You ask yourself.\n")
+    print("Suddenly the Moonchild runs away into the hills and dissapears behind one soon after...\n\nSo, now what? You ask yourself.")
     time.sleep(8)
 
 def redNightmare():
@@ -75,12 +74,30 @@ def redNightmare():
     time.sleep(1)
     print(".")
     time.sleep(2)
-    print("You open your eyes and vaguely remember a Red Nightmare about an aeroplane, you sweating, and a greyhound...")
+    print("You open your eyes and vaguely remember a Red Nightmare about an aeroplane, you sweating, and a greyhound...?")
     time.sleep(10)
-    print("You look around and remember where you are")
+    print("You look around and remember where you are.\n")
+
+def crimsonKing():
+    print("Inside the palace, it doesn't look mutch different: red bricks.\nYou walk through halls of red bricks, not knowing where to go.\nThe palace seems to be totally empty of people, but full of red bricks...")
+    time.sleep(15)
+    print("\nEventually, might be a coincidence, it might be not, you end up in big hall (of red bricks) with one singular chair in the centre of the huge hall.\nThis chair is surrounded by about ten of the same kind of guards that questioned you earlier.")
+    time.sleep(15)
+    print("\nAs soon as they hear you, all of the guards turn around.\nA group of guards starts marching towards you, while the ones left behind akwardly try to turn the chair around.\nThe guards approach menacingly, while slowly but surely the chair is turned around...")
+    time.sleep(18)
+    print("\nJust as the guards want to take you away, the chair is finally fully turned and a small naked lizard appears sitting in that very chair.\nIn a very high voice he yells: 'Stop that! Let me see the Stranger!'")
+    time.sleep(15)
+    print("\nThe guards now hastily make way for the lizard to see you.\nThe lizard's jaw drops and he says: 'Ohhh... It's you..., " + name + "...\nI'm the Crimson King, or King Crimson, if you will, but you won't know me... Nor do you know why you're here...'")
+    time.sleep(15)
+    print("A silence falls...")
+    time.sleep(5)
+    print("You stay silent, in hope of getting an answer to your question.")
+    time.sleep(5)
+    print("Finally, the King Crimson continues: 'Well... It's simple. You're not here... You're dreaming... Wake up " + name + ".'")
+    time.sleep(10)
 
 
-print("DISCLAIMER: You might have a better experience with the use of imagination and read carefully!")
+print("!!!\nDISCLAIMER: You might have a better experience with the use of imagination and read carefully!\n!!!\n")
 time.sleep(8)
 
 name = input("How would you like to name your character? ")
@@ -120,7 +137,7 @@ while answer1 != "1" or answer1 != "2" or answer1 != "3" or answer1 != "4":
         time.sleep(5)
         answer1sub1 = "0"
         while answer1sub1 != "1" or answer1sub1 != "2":
-            answer1sub1 = input("[1] You say: 'I'm sorry, but I have no idea what a Soul-Marcher is or what you mean by 'the top' (truth).\n[2] You say: That's right... I think...' (obvious lie in order to get an explanation)")
+            answer1sub1 = input("[1] You say: 'I'm sorry, but I have no idea what a Soul-Marcher is or what you mean by 'the top' (truth).\n[2] You say: That's right... I think...' (obvious lie in order to get an explanation)\n")
             if answer1sub1 == "1":
                 print("The Crawling King Snake says: \n'Right... Well neither do any of the Strangers down here. \nAll I can say that you are now in the realm of the Subterraneans and people from the top are what we call Soil-Marchers.\nI'll help your poor soul out of these godforsaken lands.\nMy Riders will take you over my highway to the west.\nThe west is the best.'\n")
                 time.sleep(20)
@@ -170,7 +187,7 @@ while answer1 != "1" or answer1 != "2" or answer1 != "3" or answer1 != "4":
         time.sleep(10)
         answer1sub4 = "0"
         while answer1sub4 != "1" or answer1sub4 != "2":
-            answer1sub4 = input("[1] You say: 'Yes, I found out that the treasure is somewhere in his kingdom, under The Willow.' (lie based on the same folklore)\n[2] You say: 'Uhm no... I'm not actually looking for a treasure, I don't know how I ended up here.' (painful truth)")
+            answer1sub4 = input("[1] You say: 'Yes, I found out that the treasure is somewhere in his kingdom, under The Willow.' (lie based on the same folklore)\n[2] You say: 'Uhm no... I'm not actually looking for a treasure, I don't know how I ended up here.' (painful truth)\n")
             if answer1sub4 == "1":
                 print("The Crawling King Snake says:\n'Alright great! I can help you with getting to the King's kingdom.\nMy Riders can take you there in the blink of an eye, let me take you to them.'\n")
                 time.sleep(15)
@@ -179,6 +196,7 @@ while answer1 != "1" or answer1 != "2" or answer1 != "3" or answer1 != "4":
                 print("The Crawling King Snake says:\n 'Of course... Just like every other Stranger that passes through these marshes.\nYou must be a Soil-Marcher, from the top.\nIf that is indeed the case, I'm gonna try to help you as best as I can.\nYou can't stay here, it's too dangerous and besides, you probably won't get out of here on you own.\nI'll get you out of these godforsaken lands, my Riders will take you over my highway to the west.\nThe west is the best.'\n")
                 time.sleep(20)
                 break
+        break
     else:
         print("Choose an available option")
         continue
@@ -233,7 +251,7 @@ while choice3 != "1" or choice3 != "2":
     if choice3 == "1":
         print("You walk.\nYou walk for a long time.\nThen... From behind the endless green hills, three towers rise up.\nA mighty palace made entirely out of red bricks appears before you.\nYou approach the gates...\n")
         time.sleep(20)
-        print("On each side of the gate stands a tall guard in medieval knight armour, both armed with a halberd.\nAs you approach the gate they cross there halberds and speak in unison:\n'Who are you and why do you wish to enter King Crimson's palace?\n")
+        print("On each side of the gate stands a tall guard in medieval knight armour, both armed with a halberd.\nAs you approach the gate they cross there halberds, forming a barrier, and speak in unison:\n'Who are you and why do you wish to enter King Crimson's palace?\n")
         time.sleep(20)
         print("You reply with: ")
         time.sleep(2)
@@ -242,7 +260,7 @@ while choice3 != "1" or choice3 != "2":
         while choice3sub1 != "1" or choice3sub1 != "2":
             choice3sub1 = input("[1] 'I come from the Southern Swamps, I'm " + name +  " and I'm looking for a way back home...' (truth)\n[2] 'I'm a free traveller and wish to speak to the King about the treasure in these lands.' (lie)\n")
             if choice3sub1 == "1":
-                print("The guards reply in unison: 'From the Southern Swamps you say... Hmpf, well the King can always use new reports from those dirty lands, come on in.'\n")
+                print("The guards reply in unison: 'From the Southern Swamps you say... Hmpf, well the King can always use new reports from those dirty lands, come on in.'")
                 time.sleep(15)
                 break
             elif choice3sub1 == "2" and liar == False:
@@ -323,22 +341,9 @@ while choice3 != "1" or choice3 != "2":
 drumRoll()
 print("\n----------------\nChapter 3: In the Court of the Crimson King\n----------------")
 
-print("Inside the palace, it doesn't look mutch different: red bricks.\nYou walk through halls of red bricks, not knowing where to go.\nThe palace seems to be totally empty of people, but full of red bricks...")
-time.sleep(15)
-print("\nEventually, might be a coincidence, it might be not, you end up in big hall (of red bricks) with one singular chair in the centre of the huge hall.\nThis chair is surrounded by about ten of the same kind of guards that questioned you earlier.")
-time.sleep(15)
-print("\nAs soon as they hear you, all of the guards turn around.\nA group of guards starts marching towards you, while the ones left behind akwardly try to turn the chair around.\nThe guards approach menacingly, while slowly but surely the chair is turned around...")
-time.sleep(18)
-print("\nJust as the guards want to take you away, the chair is finally fully turned and a small naked lizard appears sitting in that very chair.\nIn a very high voice he yells: 'Stop that! Let me see the Stranger!'")
-time.sleep(15)
-print("\nThe guards now hastily make way for the lizard to see you.\nThe lizard's jaw drops and he says: 'Ohhh... It's you..., " + name + "...\nI'm the Crimson King, or King Crimson, if you will, but you won't know me... Nor do you know why you're here...'")
-time.sleep(15)
-print("A silence falls...")
-time.sleep(5)
-print("You stay silent, in hope of getting an answer to your question.")
-time.sleep(5)
-print("Finally, the King Crimson continues: 'Well... It's simple. You're not here... You're dreaming... Wake up " + name + ".'")
-time.sleep(10)
+crimsonKing()
 
 drumRoll()
 print("===============\n\nThis is The End: beautiful friend...\n\n===============")
+time.sleep(10)
+quit()
